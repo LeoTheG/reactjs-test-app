c3c30ff023f0f9766fa6d046b23a01df2032c689
Leonar Gharib <leonargharib@gmail.com>
2017-12-29 18:34:49 -0800

    Added reactive search

--- src/App.js
+++ src/App.js
@@ -20,23 +20,42 @@ const list = [
   objectID: 1,
 },
 ];
+
+function isSearched(searchTerm){
+  return function(item) {
+    return item.title.toLowerCase().includes(searchTerm.toLowerCase());
+  }
+}
+
 class App extends Component {
   constructor(props){
     super(props);
     this.state={
       list: list,
+      searchTerm: '',
     };
     this.onDismiss = this.onDismiss.bind(this);
+    this.onSearchChange = this.onSearchChange.bind(this);
   }
   onDismiss(id){
     const isNotId = item => item.objectID !== id;
     const updatedList = this.state.list.filter(isNotId);
     this.setState({ list: updatedList });
   }
+  onSearchChange(event){
+   this.setState({searchTerm:event.target.value});
+  }
   render() {
     return (
       <div className="App">
-        {this.state.list.map(item=>
+        <form>
+          <input
+            type="text"
+            onChange={this.onSearchChange}
+          />
+        </form>
+
+        {this.state.list.filter(isSearched(this.state.searchTerm)).map(item=>
           <div key={item.objectID}>
             <span>
               <a href={item.url}>{item.title}</a>
@@ -45,7 +64,10 @@ class App extends Component {
             <span>{item.num_comments}</span>
             <span>{item.points}</span>
             <span>
-              <button onClick={()=> this.onDismiss(item.objectID)} type="button">
+              <button
+                onClick={()=> this.onDismiss(item.objectID)}
+                type="button"
+              >
                 Dismiss
               </button>
             </span>
